26f5d9a75f311d0b055d96170797d85b2aaf8e59
Olle Linderos <me@ollelinderos.com>
2017-10-22 19:34:01 +0200

    Organizing

--- configs/plugins.el
+++ configs/plugins.el
@@ -1,6 +1,4 @@
-;;
-;; Plugin management
-;;
+;; package archives
 (require 'package)
   (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/"))
   (add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/"))
@@ -17,69 +15,163 @@
 (setq use-package-always-ensure t
       use-package-always-defer t)
 
+;; which-key
 (use-package which-key :demand)
+(setq which-key-idle-delay 0)
+(which-key-mode)
+
+;; yasnippet
 (use-package yasnippet)
 (require 'yasnippet)
+(yas-global-mode)
+
+;; company
 (use-package company)
 (require 'company)
+(add-hook 'after-init-hook 'global-company-mode)
+(setq company-idle-delay t)
+
+;; evil multiple cursors
 (use-package evil-mc)
 (require 'evil-mc)
+(global-evil-mc-mode 1)
+
+;; with-editor
 (use-package with-editor)
 (require 'with-editor)
+
+;; golden ratio
 (use-package golden-ratio)
 (require 'golden-ratio)
+
+;; ag
 (use-package ag)
 (require 'ag)
+
+;; rainbow delimiters
 (use-package rainbow-delimiters)
 (require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(set-face-foreground 'rainbow-delimiters-depth-1-face "white")
+(set-face-foreground 'rainbow-delimiters-depth-2-face "cyan")
+(set-face-foreground 'rainbow-delimiters-depth-3-face "yellow")
+(set-face-foreground 'rainbow-delimiters-depth-4-face "green")
+(set-face-foreground 'rainbow-delimiters-depth-5-face "orange")
+(set-face-foreground 'rainbow-delimiters-depth-6-face "purple")
+(set-face-foreground 'rainbow-delimiters-depth-7-face "white")
+(set-face-foreground 'rainbow-delimiters-depth-8-face "cyan")
+(set-face-foreground 'rainbow-delimiters-depth-9-face "yellow")
+(set-face-foreground 'rainbow-delimiters-unmatched-face "red")
+
+;; php-mode
 (use-package php-mode)
 (require 'php-mode)
-(use-package web-mode)
-(require 'web-mode)
-(use-package js2-mode)
-(require 'js2-mode)
-(use-package which-key)
-(require 'which-key)
-(use-package swoop)
-(require 'swoop)
-(use-package smex)
-(require 'smex)
-(use-package smart-mode-line)
-(require 'smart-mode-line)
+
+;; php-extras
 (use-package php-extras)
 (require 'php-extras)
+
+;; web-mode
+(use-package web-mode)
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+  (emmet-mode)
+)
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
+;; js2-mode
+(use-package js2-mode)
+(require 'js2-mode)
+(add-hook 'js2-mode-hook
+            (lambda ()
+              (set (make-local-variable 'company-backends)
+                   '((company-yasnippet)))))
+
+;; swoop
+(use-package swoop)
+(require 'swoop)
+
+;; smex
+(use-package smex)
+(require 'smex)
+(smex-initialize)
+
+;; smart-mode-line
+(use-package smart-mode-line)
+(require 'smart-mode-line)
+(setq sml/no-confirm-load-theme t)
+(setq sml/theme 'respectful)
+(sml/setup)
+
+;; nord-theme
 (use-package nord-theme)
 (require 'nord-theme)
+(setq nord-comment-brightness 15)
+(load-theme 'nord t)
+
+;; neotree
 (use-package neotree)
 (require 'neotree)
+(setq neo-window-width 30)
+(setq neo-smart-open t)
+(add-hook 'neotree-mode-hook
+            (lambda ()
+              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
+
+;; linum-relative
 (use-package linum-relative)
 (require 'linum-relative)
+(linum-relative-mode)
+(add-hook 'prog-mode-hook 'linum-mode)
+(add-hook 'org-mode-hook 'linum-mode)
+
+;; emmet
 (use-package emmet-mode)
 (require 'emmet-mode)
+
 ;; (require 'company-tern)
+
+;; better-defaults
 (use-package better-defaults)
 (require 'better-defaults)
+
+;; smart-tabs-mode
 (use-package smart-tabs-mode)
 (require 'smart-tabs-mode)
+
+;; git-gutter
 (use-package git-gutter)
 (require 'git-gutter)
+
+;; git-gutter-fringe
 (use-package git-gutter-fringe)
 (require 'git-gutter-fringe)
-(use-package which-key :demand)
-(require 'which-key)
+(global-git-gutter-mode)
+(setq-default right-fringe-width 20)
+(setq git-gutter-fr:side 'right-fringe)
+
+;; projectile
 (use-package projectile)
 (require 'projectile)
-(use-package autopair)
-(require 'autopair)
-(use-package persp-mode)
-(require 'persp-mode)
-
 (setq projectile-indexing-method 'native)
 (setq projectile-enable-caching t)
 
-;; 
+;; autopair
+(use-package autopair)
+(require 'autopair)
+(autopair-global-mode)
+
+;; persp-mode
+(use-package persp-mode)
+(require 'persp-mode)
+(persp-mode)
+
 ;; Evil mode 
-;; 
 (setq evil-want-C-u-scroll t) ;; vim scrolling
 (use-package evil
   :demand
@@ -96,6 +188,7 @@
 (define-key evil-normal-state-map (kbd "C-u") 'evil-scroll-up)
 (define-key evil-visual-state-map (kbd "C-u") 'evil-scroll-up)
 (define-key evil-insert-state-map (kbd "C-u")
+
   (lambda ()
     (interactive)
     (evil-delete (point-at-bol) (point))))
@@ -116,112 +209,9 @@
   "p" 'projectile-command-map
   "w" 'evil-window-map)
 
-
-;;
-;; Initialize packages and hooks
-;; 
-(which-key-mode)
-(smex-initialize)
-;; (projectile-global-mode)
-(autopair-global-mode)
-(linum-relative-mode)
-(persp-mode)
-(global-evil-mc-mode 1)
-(global-git-gutter-mode)
-(yas-global-mode)
-
 (add-hook 'prog-mode-hook 'column-number-mode)
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'org-mode-hook 'linum-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
 
-(setq which-key-idle-delay 0)
-
-(setq-default right-fringe-width 20)
-(setq git-gutter-fr:side 'right-fringe)
-
-;; Rainbow delimiters
-(set-face-foreground 'rainbow-delimiters-depth-1-face "white")
-(set-face-foreground 'rainbow-delimiters-depth-2-face "cyan")
-(set-face-foreground 'rainbow-delimiters-depth-3-face "yellow")
-(set-face-foreground 'rainbow-delimiters-depth-4-face "green")
-(set-face-foreground 'rainbow-delimiters-depth-5-face "orange")
-(set-face-foreground 'rainbow-delimiters-depth-6-face "purple")
-(set-face-foreground 'rainbow-delimiters-depth-7-face "white")
-(set-face-foreground 'rainbow-delimiters-depth-8-face "cyan")
-(set-face-foreground 'rainbow-delimiters-depth-9-face "yellow")
-(set-face-foreground 'rainbow-delimiters-unmatched-face "red")
-
-;; Neo-tree 
-(setq neo-window-width 30)
-(setq neo-smart-open t)
-(add-hook 'neotree-mode-hook
-            (lambda ()
-              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
-
-;;
-;; Theming
-;; 
-(setq nord-comment-brightness 15)
-(load-theme 'nord t)
-
-(setq sml/no-confirm-load-theme t)
-(setq sml/theme 'respectful)
-(sml/setup)
-
-
-;;
-;; Initialize packages and hooks
-;; 
-(which-key-mode)
-(smex-initialize)
-;; (projectile-global-mode)
-(autopair-global-mode)
-(linum-relative-mode)
-(persp-mode)
-(global-evil-mc-mode 1)
-(global-git-gutter-mode)
-(yas-global-mode)
-
-(add-hook 'prog-mode-hook 'column-number-mode)
-(add-hook 'prog-mode-hook 'linum-mode)
-(add-hook 'org-mode-hook 'linum-mode)
-(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
-
-(setq which-key-idle-delay 0)
-
-(setq-default right-fringe-width 20)
-(setq git-gutter-fr:side 'right-fringe)
-
-;; Rainbow delimiters
-(set-face-foreground 'rainbow-delimiters-depth-1-face "white")
-(set-face-foreground 'rainbow-delimiters-depth-2-face "cyan")
-(set-face-foreground 'rainbow-delimiters-depth-3-face "yellow")
-(set-face-foreground 'rainbow-delimiters-depth-4-face "green")
-(set-face-foreground 'rainbow-delimiters-depth-5-face "orange")
-(set-face-foreground 'rainbow-delimiters-depth-6-face "purple")
-(set-face-foreground 'rainbow-delimiters-depth-7-face "white")
-(set-face-foreground 'rainbow-delimiters-depth-8-face "cyan")
-(set-face-foreground 'rainbow-delimiters-depth-9-face "yellow")
-(set-face-foreground 'rainbow-delimiters-unmatched-face "red")
-
-;; Neo-tree 
-(setq neo-window-width 30)
-(setq neo-smart-open t)
-(add-hook 'neotree-mode-hook
-            (lambda ()
-              (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter)))
-
-;;
-;; Theming
-;; 
-(setq nord-comment-brightness 15)
-(load-theme 'nord t)
-
-(setq sml/no-confirm-load-theme t)
-(setq sml/theme 'respectful)
-(sml/setup)
-
+;; magit
 (add-to-list 'load-path "~/.emacs.d/site-lisp/magit/lisp")
 (require 'magit)
 
@@ -230,41 +220,3 @@
   (add-to-list 'Info-directory-list
                "~/.emacs.d/site-lisp/magit/Documentation/"))
 
-;;
-;; Autocomplete
-;; 
-(add-hook 'after-init-hook 'global-company-mode)
-(setq company-idle-delay t)
-
-;; Major modes
-;;
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-
-(add-hook 'js2-mode-hook
-            (lambda ()
-              (set (make-local-variable 'company-backends)
-                   '((company-yasnippet)))))
-
-(defun my-web-mode-hook ()
-  "Hooks for Web mode."
-  (emmet-mode)
-)
-(add-hook 'web-mode-hook  'my-web-mode-hook)
-
-(defun my-setup-indent (n)
-  (setq c-basic-offset n)
-  (setq coffee-tab-width n) ; coffeescript
-  (setq javascript-indent-level n) ; javascript-mode
-  (setq js-indent-level n) ; js-mode
-  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
-  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
-  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
-  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
-  (setq css-indent-offset n) ; css-mode
-  (setq web-mode-style-padding n)
-  (setq web-mode-script-padding n)
-  )
-(my-setup-indent 4)
---
+++ configs/preferences.el
@@ -0,0 +1,14 @@
+(defun my-setup-indent (n)
+  (setq c-basic-offset n)
+  (setq coffee-tab-width n) ; coffeescript
+  (setq javascript-indent-level n) ; javascript-mode
+  (setq js-indent-level n) ; js-mode
+  (setq js2-basic-offset n) ; js2-mode, in latest js2-mode, it's alias of js-indent-level
+  (setq web-mode-markup-indent-offset n) ; web-mode, html tag in html file
+  (setq web-mode-css-indent-offset n) ; web-mode, css in html file
+  (setq web-mode-code-indent-offset n) ; web-mode, js code in html file
+  (setq css-indent-offset n) ; css-mode
+  (setq web-mode-style-padding n)
+  (setq web-mode-script-padding n)
+  )
+(my-setup-indent 4)
