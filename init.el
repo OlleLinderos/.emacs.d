e3dc3d758b1e65c62c1085d0beaebbcfc89401ad
Olle Linderos <me@ollelinderos.com>
2017-10-26 10:10:07 +0200

    Update init.el

--- init.el
+++ init.el
@@ -5,6 +5,9 @@
 (load "~/.emacs.d/configs/hotkeys.el")
 (load "~/.emacs.d/configs/preferences.el")
 
+;; (set-frame-parameter (selected-frame) 'alpha '(97 . 97))
+;; (add-to-list 'default-frame-alist '(alpha . (97 . 97)))
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -13,10 +16,16 @@
  '(custom-safe-themes
    (quote
     ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
- '(neo-theme (quote asci))
+ '(neo-theme (quote icons))
  '(package-selected-packages
    (quote
-    (yasnippet magit-popup magit git-gutter-fringe smart-tabs-mode evil-mc golden-ratio ag rainbow-delimiters php-mode company web-mode js2-mode which-key use-package swoop smex smart-mode-line php-extras nord-theme neotree linum-relative evil-leader emmet-mode company-tern better-defaults autopair)))
+    (shell-pop avy exec-path-from-shell counsel-projectile counsel swiper yasnippet magit-popup magit git-gutter-fringe smart-tabs-mode evil-mc golden-ratio ag rainbow-delimiters php-mode company web-mode js2-mode which-key use-package smart-mode-line php-extras nord-theme neotree linum-relative evil-leader emmet-mode company-tern better-defaults autopair)))
+ '(shell-pop-shell-type
+   (quote
+    ("ansi-term" "*ansi-term*"
+     (lambda nil
+       (ansi-term shell-pop-term-shell)))))
+ '(shell-pop-window-size 30)
  '(sml/projectile-replacement-format "[%s] ")
  '(sml/theme (quote respectful)))
 (custom-set-faces
