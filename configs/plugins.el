1e5845228b753b346c89935e3e8e741342105ff1
Olle Linderos <me@ollelinderos.com>
2017-10-31 20:37:32 +0100

    Added Melpa Stable package archive

--- configs/plugins.el
+++ configs/plugins.el
@@ -1,9 +1,10 @@
 ;; package archives
 (require 'package)
-  (add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/"))
-  (add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/"))
-  (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/"))
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa"        . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu"          . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
