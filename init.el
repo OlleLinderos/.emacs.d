0492428d2cfc53108485ecda01101dba7a8d4d84
Olle Linderos <me@ollelinderos.com>
2017-10-31 09:51:15 +0100

    merge with work config

--- init.el
+++ init.el
@@ -5,9 +5,6 @@
 (load "~/.emacs.d/configs/hotkeys.el")
 (load "~/.emacs.d/configs/preferences.el")
 
-;; (set-frame-parameter (selected-frame) 'alpha '(97 . 97))
-;; (add-to-list 'default-frame-alist '(alpha . (97 . 97)))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,11 +12,11 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
+    ("c4bd8fa17f1f1fc088a1153ca676b1e6abc55005e72809ad3aeffb74bd121d23" "70f5a47eb08fe7a4ccb88e2550d377ce085fedce81cf30c56e3077f95a2909f2" "5a0eee1070a4fc64268f008a4c7abfda32d912118e080e18c3c865ef864d1bea" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" default)))
  '(neo-theme (quote icons))
  '(package-selected-packages
    (quote
-    (shell-pop avy exec-path-from-shell counsel-projectile counsel swiper yasnippet magit-popup magit git-gutter-fringe smart-tabs-mode evil-mc golden-ratio ag rainbow-delimiters php-mode company web-mode js2-mode which-key use-package smart-mode-line php-extras nord-theme neotree linum-relative evil-leader emmet-mode company-tern better-defaults autopair)))
+    (dumb-jump shell-pop avy exec-path-from-shell counsel-projectile counsel swiper yasnippet magit-popup magit git-gutter-fringe smart-tabs-mode evil-mc golden-ratio ag rainbow-delimiters php-mode company web-mode js2-mode which-key use-package smart-mode-line php-extras nord-theme neotree linum-relative evil-leader emmet-mode company-tern better-defaults autopair)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
