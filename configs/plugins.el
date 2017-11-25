04f51c53cb56b15824c9bdb42c7e3a740c657e6b
Olle Linderos <me@ollelinderos.com>
2017-11-25 15:22:25 +0100

    ;; MELPA STABLE

--- configs/plugins.el
+++ configs/plugins.el
@@ -1,7 +1,7 @@
 ;; package archives
 (require 'package)
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
-;;(add-to-list 'package-archives '("melpa"        . "http://melpa.org/packages/"))
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa"        . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("gnu"          . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("org"          . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives '("marmalade"    . "http://marmalade-repo.org/packages/"))
