636397fd34c4b8d8158f74a2aa3fdfed5a842cfe
Olle Linderos <me@ollelinderos.com>
2018-11-18 12:58:52 +0100

    Neo-theme

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(neo-theme 'ascii)
  '(package-selected-packages
    '(paredit evil-leader evil autopair projectile git-gutter-fringe git-gutter better-defaults linum-relative neotree smart-mode-line avy ivy shell-pop rainbow-delimiters ag golden-ratio with-editor company which-key use-package))
  '(shell-pop-shell-type
