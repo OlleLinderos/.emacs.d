b12a5df35854d9aab2bb24fcbedcd3f2e84062ab
Olle Linderos <me@ollelinderos.com>
2018-12-05 15:15:21 +0100

    Branching off

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  '(neo-theme (quote nerd))
  '(package-selected-packages
    (quote
-    (ng2-mode company-web ranger paredit evil-leader evil autopair projectile git-gutter-fringe git-gutter better-defaults linum-relative neotree smart-mode-line avy ivy shell-pop rainbow-delimiters ag golden-ratio with-editor company which-key use-package)))
+    (company-web ranger paredit evil-leader evil autopair projectile git-gutter-fringe git-gutter better-defaults linum-relative neotree smart-mode-line avy ivy shell-pop rainbow-delimiters ag golden-ratio with-editor company which-key use-package)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
@@ -20,6 +20,6 @@
  ;; If there is more than one, they won't work right.
  '(css-selector ((t (:inherit font-lock-function-name-face))))
  '(js2-object-property ((t (:foreground "LightSteelBlue2"))))
- '(mode-line ((t (:background "#4C566A" :foreground "#88C0D0" :inverse-video nil))))
  '(persp-selected-face ((t (:foreground "FF9900"))))
+ '(sml/minor-modes ((t (:foreground "#778899" :width normal))))
  '(tide-hl-identifier-face ((t (:inherit highlight :foreground "#ff9900")))))
