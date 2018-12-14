b653ff90a1aba9316654f8779c575b6edabac1bd
Olle Linderos <me@ollelinderos.com>
2018-12-14 13:15:04 +0100

    Indentation is important

--- init.el
+++ init.el
@@ -5,17 +5,17 @@
 
 (let ((file-name-handler-alist nil)
       (gc-cons-threshold most-positive-fixnum))
-	(require 'packages)  ;; setup packages
-	(require 'general)   ;; my own defaults
-        (require 'theme)     ;; theming settings
-        (require 'editor)    ;; inside prog buffer
-        (require 'ide)       ;; ide tools
-        (require 'clojure)   ;; everything clojure
-        (require 'web)       ;; webmode (html, css, inline js)
-        (require 'tsjs)      ;; typescript & javascript
-	(require 'hotkeys)   ;; hotkeys
-	(require 'utility)   ;; custom utility functions
-        )
+  (require 'packages) ;; setup packages
+  (require 'general)  ;; my own defaults
+  (require 'theme)    ;; theming settings
+  (require 'editor)   ;; inside prog buffer
+  (require 'ide)      ;; ide tools
+  (require 'clojure)  ;; everything clojure
+  (require 'web)      ;; webmode (html, css, inline js)
+  (require 'tsjs)     ;; typescript & javascript
+  (require 'hotkeys)  ;; hotkeys
+  (require 'utility)  ;; custom utility functions
+  )
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file 'noerror)
