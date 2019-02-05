c02cf67b7e1d19dad45cf2a403fe452180d1544d
Olle Linderos <me@ollelinderos.com>
2019-02-05 12:51:20 +0100

    Changed background colors on fringe, modeline, default & term

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-enabled-themes (quote (smart-mode-line-respectful)))
  '(global-git-gutter-mode t)
  '(js2-highlight-level 3)
- '(neo-theme (quote icons) t)
+ '(neo-theme (quote icons))
  '(package-selected-packages
    (quote
     (multi-term ranger paredit evil-leader evil autopair projectile git-gutter-fringe git-gutter better-defaults linum-relative neotree smart-mode-line avy ivy shell-pop rainbow-delimiters ag golden-ratio with-editor company which-key use-package)))
@@ -20,15 +20,18 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#232833" :foreground "#D8DEE9" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Space Mono"))))
+ '(default ((t (:inherit nil :stipple nil :background "#181b23" :foreground "#D8DEE9" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Space Mono"))))
  '(css-selector ((t (:inherit font-lock-function-name-face))))
- '(fringe ((t (:background "#232833" :foreground "#D8DEE9"))))
+ '(fringe ((t (:background "#181b23" :foreground "#D8DEE9"))))
  '(highlight ((((class color) (min-colors 89)) (:foreground "#88C0D0" :background "#434C5E"))))
  '(hl-line ((t (:background "#2f3647"))))
  '(js2-object-property ((t (:foreground "LightSteelBlue2"))))
- '(linum ((t (:background "#232833" :foreground "#4C566A"))))
- '(mode-line ((t (:background "#38414f" :foreground "#88C0D0" :inverse-video nil))))
+ '(linum ((t (:background "#181b23" :foreground "#4C566A"))))
+ '(linum-relative-current-face ((t (:background "#181b23" :foreground "#eee"))))
+ '(mode-line ((t (:background "#2c3242" :foreground "#88C0D0" :inverse-video nil))))
+ '(mode-line-inactive ((t (:background "#222733" :foreground "#D8DEE9" :inverse-video nil))))
  '(neo-root-dir-face ((((class color) (min-colors 89)) (:foreground "#8FBCBB" :weight bold))))
  '(persp-selected-face ((t (:foreground "FF9900"))))
  '(sml/minor-modes ((t (:foreground "#778899" :width normal))))
+ '(term ((t (:background "#181b23" :foreground "#D8DEE9"))))
  '(tide-hl-identifier-face ((t (:inherit highlight :foreground "#ff9900")))))
