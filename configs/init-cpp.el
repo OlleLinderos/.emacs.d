a701de2115111b3420eec5fbe0e44cfad8d80fec
Olle Linderos <me@ollelinderos.com>
2019-08-20 19:47:25 +0200

    Add support for Arduino, and irony for c++ completion

--- configs/init-cpp.el
+++ configs/init-cpp.el
@@ -1,2 +1,22 @@
+(use-package irony-eldoc)
+(use-package irony-mode) 
+(use-package platformio-mode)
+
+(add-to-list 'auto-mode-alist '("\\.ino$" . arduino-mode)) 
+
+(add-hook 'c++-mode-hook (lambda ()
+                           (irony-mode)
+                           (irony-eldoc)
+                           (platformio-conditionally-enable)))
+
+(add-hook 'irony-mode-hook
+          (lambda ()
+            (define-key irony-mode-map [remap completion-at-point]
+              'irony-completion-at-point-async)
+
+            (define-key irony-mode-map [remap complete-symbol]
+              'irony-completion-at-point-async)
+
+            (irony-cdb-autosetup-compile-options)))
 
 (provide 'init-cpp)
