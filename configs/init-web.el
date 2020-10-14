41903880c71467a235714d0b397fb3d860aba4a2
Olle Linderos <me@ollelinderos.com>
2020-10-14 11:35:05 +0200

    Nice setup for TSX with tide

--- configs/init-web.el
+++ configs/init-web.el
@@ -20,24 +20,21 @@
   :init
   (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode)) 
   (set (make-local-variable 'company-backends) '(company-web-html))
   (add-hook 'web-mode-hook 'emmet-mode)
+  (add-hook 'web-mode-hook #'setup-tide-mode)
   :config
-  (setq web-mode-markup-indent-offset 4
+  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
+  (setq web-mode-markup-indent-offset 2
         web-mode-css-indent-offset 4
-        web-mode-code-indent-offset 4
-        web-mode-block-padding 4
-        web-mode-comment-style 4
-
+        web-mode-code-indent-offset 2
+        web-mode-block-padding 2
+        web-mode-comment-style 2
         web-mode-enable-css-colorization t
         web-mode-enable-auto-pairing t
         web-mode-enable-comment-keywords t
-        web-mode-enable-current-element-highlight t)
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (when (string-equal "tsx" (file-name-extension buffer-file-name))
-		(setup-tide-mode))))
-  (flycheck-add-mode 'typescript-tslint 'web-mode))
+        web-mode-enable-current-element-highlight t))
 
 (use-package typescript-mode
   :ensure t
@@ -57,31 +54,14 @@
   (flycheck-add-mode 'typescript-tslint 'ng2-ts-mode)
   (flycheck-add-mode 'typescript-tide 'ng2-ts-mode))
 
-(use-package ng2-mode
-  :ensure)
-
-(use-package js2-mode
-  :ensure t
-  :interpreter (("node" . js2-mode))
-  :bind (:map js2-mode-map ("C-c C-p" . js2-print-json-path))
-  :mode "\\.\\(js\\|json\\)$"
-  :config
-  (add-hook 'js-mode-hook 'js2-minor-mode)
-  (setq js2-basic-offset 2
-        js2-highlight-level 3
-        js2-mode-show-parse-errors nil
-        js2-mode-show-strict-warnings nil))
-
 (evil-leader/set-key "j" 'javascript-map)
+
 (progn
   (define-prefix-command 'javascript-map)
   (define-key javascript-map (kbd "a") 'angular-map)
-  (define-key javascript-map (kbd "t") 'tide-map))
-
-(progn
-  (define-prefix-command 'tide-map)
-  (define-key tide-map (kbd "d") 'tide-jump-to-definition)
-  (define-key tide-map (kbd "r") 'tide-rename-symbol))
+  
+  (define-key javascript-map (kbd "d") 'tide-jump-to-definition)
+  (define-key javascript-map (kbd "r") 'tide-rename-symbol))
 
 (progn
   (define-prefix-command 'angular-map)
