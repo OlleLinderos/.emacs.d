fd27436b4d3e0e77d86b36ff6b2dc250061bfd5a
Olle Linderos <olle@linderos.nu>
2021-05-15 17:06:25 +0200

    Get rid of bloat

--- lisp/setup-theme.el
+++ lisp/setup-theme.el
@@ -1,7 +1,8 @@
 (when (member "Droid Sans Mono" (font-family-list))
   (set-face-attribute 'default nil :font "Droid Sans Mono" :height 143))
 
-(setq ns-use-proxy-icon nil
+(setq custom-safe-themes t
+      ns-use-proxy-icon nil
       frame-title-format nil
       inhibit-splash-screen t
       initial-scratch-message nil
@@ -13,26 +14,13 @@
 
               Hello my old friend")
 
-(if (display-graphic-p)
-    (progn
-      (setq default-frame-alist
-            '((top . 30)
-              (left . 29)
-              (alpha . (100 . 100))
-              (ns-transparent-titlebar . t)
-              (tool-bar-lines . 0)
-              (width . 135)
-              (height . 39)))))
-
 (use-package smart-mode-line
   :init
-  (setq sml/no-confirm-load-theme t)
-  (setq sml/theme 'respectful)
+  (setq sml/no-confirm-load-theme t
+        sml/theme 'respectful)
   (sml/setup))
 
 (use-package all-the-icons)
-
-(setq custom-safe-themes t)
 
 (require 'atom-one-dark-theme)
 (add-hook 'after-init-hook (lambda ()
@@ -40,4 +28,4 @@
 
 (set-face-attribute 'fringe nil :background nil)
 
-(provide 'theme)
+(provide 'setup-theme)
