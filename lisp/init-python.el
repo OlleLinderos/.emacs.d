974f79fb903ee5dab8c0c7109fe2b1d74fe1b97c
Olle Linderos <olle@linderos.nu>
2021-05-15 18:24:35 +0200

    Use `lsp-python-ms` for everything python

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,12 +1,26 @@
-;; (use-package python-mode
-;;   :hook ((python-mode . (lambda ()
-;;                           (when (require 'lsp-python nil t)
-;;                             (lsp))))))
+(use-package lsp-python-ms
+  :config
+  (setq lsp-pyls-plugins-flake8-enabled t)
+  (lsp-register-custom-settings
+   '(("pyls.plugins.pyls_mypy.enabled" t t)
+     ("pyls.plugins.pyls_mypy.live_mode" nil t)
+     ("pyls.plugins.pyls_black.enabled" t t)
+     ("pyls.plugins.pyls_isort.enabled" t t)
+
+     ;; Disable these as they're duplicated by flake8
+     ("pyls.plugins.pycodestyle.enabled" nil t)
+     ("pyls.plugins.mccabe.enabled" nil t)
+     ("pyls.plugins.pyflakes.enabled" nil t)))
+  :init (setq lsp-python-ms-auto-install-server t)
+  :hook (python-mode . (lambda ()
+                         (flycheck-add-next-checker 'lsp '(warning . python-pylint))
+                          (require 'lsp-python-ms)
+                          (lsp))))
 
 (use-package pyvenv
   :demand t
   :config
-  (setq pyvenv-workon "emacs")  ; Default venv
-  (pyvenv-tracking-mode 1))  ; Automatically use pyvenv-workon via dir-locals
+  (setq pyvenv-workon "emacs")
+  (pyvenv-tracking-mode 1))
 
 (provide 'init-python)
