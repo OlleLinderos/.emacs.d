01c3ba5965f0ee2db859256506626e52d6cf7a27
Olle Linderos <olle@linderos.nu>
2021-05-15 19:55:15 +0200

    Use LSP for Go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -1,5 +1,9 @@
 (use-package go-mode
-   :mode "\\.go\\'"
-   :hook (before-save . gofmt-before-save))
+  :defer t
+  :hook ((go-mode . (lambda ()
+                      (add-hook 'before-save-hook #'gofmt-before-save nil t)))
+         (go-mode . lsp-deferred))
+  :ensure-system-package (go
+                          (gopls . "GO111MODULE=on go get golang.org/x/tools/gopls@latest")))
 
 (provide 'init-go)
