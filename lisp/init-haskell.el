f11e6403c8babf4b8a1d69ce12cd1f33094f31db
Olle Linderos <olle@linderos.nu>
2021-05-15 19:54:47 +0200

    Use LSP for Haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,24 +1,7 @@
-(use-package haskell-mode
-  :pin melpa-stable)
-
-;; (use-package company-ghc)
-
-;; (autoload 'ghc-init "ghc" nil t)
-;; (autoload 'ghc-debug "ghc" nil t)
-;; (add-hook 'haskell-mode-hook (lambda () (ghc-init)))
-
-;; (add-to-list 'company-backends 'company-ghc)
-;; (custom-set-variables '(company-ghc-show-info t))
-
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'haskell-indent-mode)
-(add-hook 'haskell-mode-hook 'haskell-font-lock-symbols t)
-
-(put 'downcase-region 'disabled nil)
-
-(evil-leader/set-key "h" 'haskell-map)
-(progn
-  (define-prefix-command 'haskell-map)
-  (define-key haskell-map (kbd "e") 'inferior-haskell-load-and-run))
+(use-package lsp-haskell
+  :ensure-system-package (haskell-language-server-wrapper . "yay -S haskell-language-server-bin")
+  :init
+  (add-hook 'haskell-mode-hook #'lsp)
+  (add-hook 'haskell-literate-mode-hook #'lsp))
 
 (provide 'init-haskell)
