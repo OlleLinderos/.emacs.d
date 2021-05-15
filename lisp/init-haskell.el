e0ee756f067bb8ad64f36e9ef8cb32d984c774cf
Olle Linderos <olle@linderos.nu>
2021-05-15 20:59:02 +0200

    Don't ensure haskell-language-server all the time

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -1,5 +1,5 @@
 (use-package lsp-haskell
-  :ensure-system-package (haskell-language-server-wrapper . "yay -S haskell-language-server-bin")
+  ;:ensure-system-package (haskell-language-server-wrapper . "yay -S haskell-language-server-bin")
   :init
   (add-hook 'haskell-mode-hook #'lsp)
   (add-hook 'haskell-literate-mode-hook #'lsp))
