44c64bd79b2a5d7d8353e730e5c31bc478aa16b3
Olle Linderos <olle@zimpler.com>
2022-11-11 13:12:57 +0100

    Set up pyright with pyvenv

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,32 +1,21 @@
 (use-package lsp-pyright
+  :hook
+  (python-mode . (lambda ()
+                   (require 'lsp-pyright)
+                   (lsp-deferred))))
+
+(use-package pyvenv
   :ensure t
-  :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp))))  ; or lsp-deferred
+  :init
+  (setenv "WORKON_HOME" "~/.venvs/")
+  :config
+  (pyvenv-mode t)
 
-;; (use-package lsp-python-ms
-;;   :config
-;;   (setq lsp-pyls-plugins-flake8-enabled t)
-;;   (lsp-register-custom-settings
-;;    '(("pyls.plugins.pyls_mypy.enabled" t t)
-;;      ("pyls.plugins.pyls_mypy.live_mode" nil t)
-;;      ("pyls.plugins.pyls_black.enabled" t t)
-;;      ("pyls.plugins.pyls_isort.enabled" t t)
-
-;;      ;; Disable these as they're duplicated by flake8
-;;      ("pyls.plugins.pycodestyle.enabled" nil t)
-;;      ("pyls.plugins.mccabe.enabled" nil t)
-;;      ("pyls.plugins.pyflakes.enabled" nil t)))
-;;   :init (setq lsp-python-ms-auto-install-server t)
-;;   :hook (python-mode . (lambda ()
-;;                          (flycheck-add-next-checker 'lsp '(warning . python-pylint))
-;;                           (require 'lsp-python-ms)
-;;                           (lsp))))
-
-;; (use-package pyvenv
-;;   :demand t
-;;   :config
-;;   (setq pyvenv-workon "emacs")
-;;   (pyvenv-tracking-mode 1))
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
 
 (provide 'init-python)
