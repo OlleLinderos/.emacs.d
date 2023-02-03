8c9a72a7b12d27e93f6f888ff36c8042db0eed55
Olle Linderos <olle@zimpler.com>
2023-02-03 16:10:16 +0100

    Add init-ts

---
+++ lisp/init-ts.el
@@ -0,0 +1,10 @@
+(use-package typescript-mode
+  :after tree-sitter
+  :hook (typescript-mode . lsp-deferred)
+  :config
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+(provide 'init-ts)
